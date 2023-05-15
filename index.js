53a60765b2f4483f6ec73e7a5b5014c39172fe06
Luferat <contato@luferat.net>
2023-05-15 10:05:59 -0300

    Ajustes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+/**
+ * FrontEndeiros API 1.0
+ * /index.js - Aplicativo principal
+ * By Luferat
+ * MIT License 2023 
+ **/
+
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-
 server.use(middlewares)
 server.use(router)
-server.listen(80, () => {
-  console.log('JSON Server is running')
-})
+server.listen(80, () => { console.log('API fake is running') })
